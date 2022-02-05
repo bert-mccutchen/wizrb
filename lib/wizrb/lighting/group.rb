13cb826114876f391d7b8b9b47361eeb2f2615b4
Bert McCutchen <bmccutchen@infotech.com>
2022-02-04 22:20:00 -0500

    Added convenience methods to lighting groups

--- lib/wizrb/lighting/group.rb
+++ lib/wizrb/lighting/group.rb
@@ -5,6 +5,33 @@ require_relative '../shared/group'
 module Wizrb
   module Lighting
     class Group < Wizrb::Shared::Group
+      def brightness(value)
+        dispatch_event(Wizrb::Lighting::Events::SetBrightnessEvent.new(value))
+      end
+
+      def cold_white(value)
+        dispatch_event(Wizrb::Lighting::Events::SetColdWhiteEvent.new(value))
+      end
+
+      def color_temp(value)
+        dispatch_event(Wizrb::Lighting::Events::SetColorTempEvent.new(value))
+      end
+
+      def rgb(red, green, blue)
+        dispatch_event(Wizrb::Lighting::Events::SetRgbEvent.new(red, green, blue))
+      end
+
+      def speed(value)
+        dispatch_event(Wizrb::Lighting::Events::SetSpeedEvent.new(value))
+      end
+
+      def warm_white(value)
+        dispatch_event(Wizrb::Lighting::Events::SetWarmWhiteEvent.new(value))
+      end
+
+      def scene(value)
+        dispatch_event(Wizrb::Lighting::Events::SetSceneEvent.new(value))
+      end
     end
   end
 end
